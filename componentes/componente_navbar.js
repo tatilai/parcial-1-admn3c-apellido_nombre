64a22a1b1113bfa6d395b049579d80a596103f2b
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-26 19:03:21 -0300

    Update componente_navbar.js

--- componentes/componente_navbar.js
+++ componentes/componente_navbar.js
@@ -1,1 +1,8 @@
-Vue.component('componente-navbar') 
+Vue.component('componente-navbar',{
+    template:`<div>
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <img src="img/gestion-de-proyectos.png></img>
+    </a>
+     `
+}) 
