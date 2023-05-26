bfdb1d6d8fb2d271c6d1e3086d3aed852ac87286
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-26 19:13:26 -0300

    Update componente_navbar.js

--- componentes/componente_navbar.js
+++ componentes/componente_navbar.js
@@ -6,7 +6,7 @@ Vue.component('componente-navbar',{
     </a>
 
     <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-    <li><a href="index.html" class="nav-link px-2 link-dark">gestión</a></li>
+    <li><a href="index.html" class="nav-link px-2 link-dark">Gestión</a></li>
     <li><a href="notas.html" class="nav-link px-2 link-dark">Proyectos</a></li>
     </ul>
     </header>
