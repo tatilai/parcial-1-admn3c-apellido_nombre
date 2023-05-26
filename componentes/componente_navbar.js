a8db5f4c1ae31bc7a6b34e4d96a01c94254b19ea
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-26 20:46:42 -0300

    Update componente_navbar.js

--- componentes/componente_navbar.js
+++ componentes/componente_navbar.js
@@ -1,4 +1,7 @@
-Vue.component('componente-navbar',{
+
+
+
+/*Vue.component('componente-navbar',{
     template:`
     <div>
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
@@ -6,12 +9,30 @@ Vue.component('componente-navbar',{
     <img src="img/gestion-de-proyectos.png></img>
     </a>
 
-    <ul style="list-style: none;" class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" >
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" >
     <li><a href="index.html" class="nav-link px-2 link-dark">Gestión</a></li>
-    <li><a href="notas.html" class="nav-link px-2 link-dark">Proyectos</a></li>
+    <li><a href="proyectos.html" class="nav-link px-2 link-dark">Proyectos</a></li>
     </ul>
     </header>
     </div>
      `
-}) 
+}) */
+
+
+Vue.component('componente-navbar', {
+    template: `
+    <div>
+      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+          <img src="img/gestion-de-proyectos.png" width="50px" height="50px"></img>
+        </a>
+  
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="index.html" class="nav-link px-2 link-dark">Gestión</a></li>
+          <li><a href="proyectos.html" class="nav-link px-2 link-dark">Proyectos</a></li>
+        </ul>
+      </header>
+    </div>
+    `
+  })
 
