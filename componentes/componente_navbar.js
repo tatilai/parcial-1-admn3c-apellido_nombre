c28659cce536aefa365e1c0d9424cdafb93808ed
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-26 20:40:40 -0300

    navbar

--- componentes/componente_navbar.js
+++ componentes/componente_navbar.js
@@ -1,11 +1,12 @@
 Vue.component('componente-navbar',{
-    template:`<div>
+    template:`
+    <div>
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-    <a class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
     <img src="img/gestion-de-proyectos.png></img>
     </a>
 
-    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" style="list-style:none;">
     <li><a href="index.html" class="nav-link px-2 link-dark">Gestión</a></li>
     <li><a href="notas.html" class="nav-link px-2 link-dark">Proyectos</a></li>
     </ul>
@@ -13,3 +14,4 @@ Vue.component('componente-navbar',{
     </div>
      `
 }) 
+
