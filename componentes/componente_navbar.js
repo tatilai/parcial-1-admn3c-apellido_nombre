14d47814679ca6fc2a862665695fc7c9c89ddc48
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-26 21:14:49 -0300

    se agrega el navbar a la segunda pagina

--- componentes/componente_navbar.js
+++ componentes/componente_navbar.js
@@ -22,7 +22,7 @@
 Vue.component('componente-navbar', {
     template: `
     <div>
-      <header class="navbar navbar-secondary d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
           <img src="img/gestion-de-proyectos.png" width="70px" height="70px"></img>
         </a>
