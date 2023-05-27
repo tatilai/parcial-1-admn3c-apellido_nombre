408b9875854dd75fca6dfb7c1573a8bae0e52af8
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-26 21:01:47 -0300

    Update componente_navbar.js

--- componentes/componente_navbar.js
+++ componentes/componente_navbar.js
@@ -24,7 +24,7 @@ Vue.component('componente-navbar', {
     <div>
       <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-          <img src="img/gestion-de-proyectos.png" width="50px" height="50px"></img>
+          <img src="img/gestion-de-proyectos.png" width="70px" height="70px"></img>
         </a>
   
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
