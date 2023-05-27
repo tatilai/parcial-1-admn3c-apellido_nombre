0025803a785a1334c8df74896f97107ef7164765
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-26 22:40:36 -0300

    ingreso de imagenes de los equipos

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var app = new Vue({
     el: "#app",
     data: {
-      titulo:'Gestor de proyectos'
+      titulo:'Gestor de proyectos',
+      imagenEquipo1: "img/equipo_1.png",
+      imagenEquipo2:"img/equipo_2.jpg"
     }
-  }) 
+  }); 
