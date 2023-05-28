7e2c74a48f4e7e594a195fe677b91c9212266775
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-28 17:32:00 -0300

    Update componente-gestion-proyectos.js

--- componentes/componente-gestion-proyectos.js
+++ componentes/componente-gestion-proyectos.js
@@ -46,10 +46,11 @@ Vue.component('componente-gestion-proyectos',{
 
        <div class="row">
        <div class="col-md-6 col-lg"> 
-        <label for="descripcion">Descripción del Proyecto:</label>
-      <textarea v-model="descripcionProyecto" id="descripcion" required></textarea>
-      </div>
-     
+       <div class="mb-3"> 
+         <label for="descripcion" class="form-label">Descripción del Proyecto:</label>
+          <textarea v-model="descripcionProyecto" class="form-control" id="descripcion" name="descripcion" required></textarea>
+      </div>      
+      </div>     
       </div>
     
    </div>
