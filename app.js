e441c3d633ba27399aadc14d8bc4add50dde3816
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-28 23:46:32 -0300

    segundo formulario

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = new Vue({
     el: "#app",
     data: {
       titulo:'Gestor de proyectos',
+      segundoTitulo:"Formulario de contacto entre los equipos",
       imagenEquipo1: "img/equipo_1.png",
       imagenEquipo2:"img/equipo_2.png"
     }
