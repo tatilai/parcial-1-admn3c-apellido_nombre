bb1fceb55a552457c4f8964fee16d62236086974
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-29 00:07:27 -0300

    Update componente-formcontacto.js

--- componentes/componente-formcontacto.js
+++ componentes/componente-formcontacto.js
@@ -1,4 +1,23 @@
 Vue.component('componente-formcontacto',{
-    
+    data:function(){
+
+        return{
+            data() {
+                return {
+                  equipos: [
+                    {
+                      nombre: '',
+                      miembros: [
+                        {
+                          nombre: '',
+                          rol: ''
+                        }
+                      ]
+                    }
+                  ]
+                }
+              }
+        }
+    },
 
 });
