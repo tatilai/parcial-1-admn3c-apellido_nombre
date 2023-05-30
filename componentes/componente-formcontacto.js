2ab0513200aa5c0f6a580412ceadd143db92ea35
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-29 23:26:53 -0300

    correcciones  de ortografia

--- componentes/componente-formcontacto.js
+++ componentes/componente-formcontacto.js
@@ -28,7 +28,7 @@ Vue.component('componente-formcontacto',{
   <div class="form-body bg-claro p-4 mt-4 w-50 mx-auto d-flex justify-content-center">
     
     <form @submit.prevent="agregarEquipo">
-    <p>Aqui le podrás dejar un mensaje a los demas miembros del equipo,incluyendo las modificaciones que le quieras hacer al proyecto</p>
+    <p style="font-weight:bold">Aquí le podrás dejar un mensaje a los demás miembros del equipo,incluyendo las modificaciones que le quieras hacer al proyecto</p>
       <div v-for="(equipo, index) in equipos" :key="index">
         <h2>Equipo {{ index + 1 }}</h2>
         <div>
