53ba2752bbfb4d708796657eb37d14373ee5a962
tatilai <99092149+tatilai@users.noreply.github.com>
2023-05-30 21:47:00 -0300

    Update componente-formcontacto.js

--- componentes/componente-formcontacto.js
+++ componentes/componente-formcontacto.js
@@ -24,53 +24,73 @@ Vue.component('componente-formcontacto',{
   template:`
   
 
-  <div class="container w-100 h-100 border bg-claro p-4 mt-4">
-  <div class="col-12 col-lg-10 p-3"> 
-    <form @submit.prevent="agregarEquipo">
-      <img src="img/trabajo.png" alt="gente sentada alrededor de una mesa" class="imagen-izquierda">
-      <div class="texto-derecha">
-        <div class="row g-2">
-          <div class="col-md">
-            <p style="font-weight:bold">Aquí le podrás dejar un mensaje a los demás miembros del equipo, incluyendo las modificaciones que le quieras hacer al proyecto</p> 
-          </div>
-          <div class="col-md">
-            <div class="form-floating mb-3"> 
-              <div v-for="(equipo, index) in equipos" :key="index">
-                <h2>Equipo {{ index + 1 }}</h2>
-                <input v-model="equipo.nombre" type="text" class="form-control"  id="nombre" name="nombre"  placeholder="Ingrese nombre del equipo">
-                <label for="nombre" class="form-label">Nombre del Equipo:</label>
-              </div>
+  <div class="form-body bg-light text-dark p-4 mt-4 w-50 mx-auto" id="equipoForm">
+  <div class="row">
+    <div class="form-holder">
+      <div class="form-content">
+        <div class="form-items">
+          <h3>Equipo</h3>
+          <p class="fw-bold">Aquí puedes dejar un mensaje al equipo y seleccionar el rol</p>
+          <form @submit.prevent="guardarEquipo" novalidate>
+            <div class="col-md-12 mb-3">
+              <label for="nombre" class="form-label">Nombre del Equipo:</label>
+              <input class="form-control my-1" v-model.lazy="equipo.nombre" type="text" name="nombre" placeholder="Ingrese nombre del equipo" required>
+              <div class="invalid-feedback" v-if="errors.nombre">{{ errors.nombre }}</div>
             </div>
-          </div>     
-        </div>
-        <div class="row"> 
-          <div class="col-md-6 col-lg">
-            <div class="mb-3">
-              <label for="comentarios" class="form-label text-salmon">Comentarios</label>
-              <textarea class="form-control" id="comentarios" name="comentarios"></textarea>
+            <div class="col-md-12 mb-3">
+              <label for="comentarios" class="form-label">Comentarios:</label>
+              <textarea class="form-control" rows="3" v-model="equipo.comentarios" name="comentarios" required></textarea>
+              <div class="invalid-feedback" v-if="errors.comentarios">{{ errors.comentarios }}</div>
             </div>
-          </div>
-          <div class="col-md"> 
-            <div class="mb-3"> 
-              <label for="fav" class="form-label" style="font-weight:bold">Miembros del Equipo:</label>     
-              <select v-for="(miembro, miembroIndex) in equipo.miembros" :key="miembroIndex" class="form-select mt-3" name="fav" id="fav" v-model="miembro.rol" required>
-                <option value="" disabled selected>Seleccionar Rol</option>
+            <div class="col-md-12 mb-3">
+              <label for="rol" class="form-label">Rol:</label>
+              <select class="form-select mt-3" v-model="equipo.rol" required>
+                <option disabled value="">Seleccionar Rol</option>
                 <option value="Líder">Líder</option>
                 <option value="Desarrollador">Desarrollador</option>
                 <option value="Diseñador">Diseñador</option>
                 <option value="Tester">Tester</option>
               </select>
-              <button type="button" @click="eliminarMiembro(index, miembroIndex)">Eliminar</button>
-            </div>      
-          </div>        
+              <div class="invalid-feedback" v-if="errors.rol">{{ errors.rol }}</div>
+            </div>
+            <div class="form-button mt-3">
+              <button id="submit" type="submit" class="btn btn-primary">Guardar Equipo</button>
+            </div>
+          </form>
         </div>
-        <button type="button" @click="agregarMiembro(index)">Agregar Miembro</button>
       </div>
-      <button type="submit">Guardar Equipos</button>
-    </form>
-  </div> 
-</div>
- 
+    </div>
+     </div>
+     <div>
+    <div v-if="enviado === true">
+      <div v-if="Object.keys(errors).length > 0" class="my-5 text-center">
+        <ul>
+          <li v-for="(error, key) in errors" :key="key">{{ error }}</li>
+        </ul>
+      </div>
+      <div v-else class="my-5 text-center">
+        <span class="alert alert-success" role="alert">Equipo guardado con éxito</span>
+      </div>
+    </div>
+    <div v-if="equipos.length > 0" class="mt-5 card w-50 mx-auto p-2 mb-2 text-dark">
+      <h2>Equipos Guardados</h2>
+      <ul v-for="equipo in equipos" :key="equipo.id" class="text-dark">
+        <li>
+          Nombre del Equipo: {{ equipo.nombre }}
+        </li>
+        <li>
+          Comentarios: {{ equipo.comentarios }}
+        </li>
+        <li>
+          Rol: {{ equipo.rol }}
+        </li>
+      </ul>
+    </div>
+
+   </div> 
+
+
+  </div>
 
    `,  
 
