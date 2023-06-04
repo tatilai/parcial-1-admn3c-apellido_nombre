da4b19271a0d3680e619a4f50c83fd4814e091aa
tatilai <99092149+tatilai@users.noreply.github.com>
2023-06-04 16:18:53 -0300

    vue-router

--- componentes/componente_navbar.js
+++ componentes/componente_navbar.js
@@ -28,7 +28,9 @@ Vue.component('componente-navbar', {
         </a>
   
         <ul class="nav  col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="index.html" class="nav-link px-2 link-dark">Gestión</a></li>
+          <li v-for="item in links">
+          <router-link to="" class="nav-link px-2 link-dark" aria-current="page">{{item.text}}</router-link>
+          </li>
           <li><a href="proyectos.html" class="nav-link px-2 link-dark">Proyectos</a></li>
         </ul>
       </header>
